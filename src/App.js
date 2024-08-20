9de27883c2b012409a4b12d508fa0f122cbbdceb
munir sebikaaka <munirsebikaaka@gmail.com>
2024-08-20 12:51:52 +0300

    feat:this is the travel park app built to practise more react skill by codesmann developers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passport", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>Far Away</h1>;
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (el, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item" />
+      <button>ADD</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>X</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer>
+      <em>you have x items on your list, and you aready packed x</em>
+    </footer>
+  );
+}
