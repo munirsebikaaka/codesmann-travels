036505f17498ffaeb8ee7c93391e138638aca0a7
munir sebikaaka <munirsebikaaka@gmail.com>
2024-08-26 12:09:15 +0300

    feat: implemented the function that deletes the list if the user wants to clear the list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ export default function App() {
   function handleDelItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
   }
+  function handleDelArr() {
+    const comfirm = window.confirm(" A u sure u want to CLEAR the list ?");
+    if (comfirm) setItems((el) => el.filter((el) => el.length));
+  }
   function hundleToggleItems(id) {
     setItems(
       items.map((item) => (item.id === id ? { ...item, packed: true } : item))
@@ -22,6 +26,7 @@ export default function App() {
         items={items}
         onHandleDelItem={handleDelItem}
         onHundleToggleItems={hundleToggleItems}
+        onHundleDelArr={handleDelArr}
       />
       <Stats items={items} />
     </div>
@@ -72,18 +77,33 @@ function Form({ handleNeeds }) {
     </form>
   );
 }
-function PackingList({ items, onHandleDelItem, onHundleToggleItems }) {
+function PackingList({
+  items,
+  onHandleDelItem,
+  onHundleToggleItems,
+  onHundleDelArr,
+}) {
   return (
-    <ul className="list">
-      {items.map((item) => (
-        <Item
-          item={item}
-          inHundleDeletes={onHandleDelItem}
-          onHundleToggleItems={onHundleToggleItems}
-          key={item.id}
-        />
-      ))}
-    </ul>
+    <div>
+      <ul className="list">
+        {items.map((item) => (
+          <Item
+            item={item}
+            inHundleDeletes={onHandleDelItem}
+            onHundleToggleItems={onHundleToggleItems}
+            key={item.id}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select>
+          <option value="inputs">Sort by input orders</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
+      <button onClick={onHundleDelArr}>Clear list</button>
+    </div>
   );
 }
 function Item({ item, inHundleDeletes, onHundleToggleItems }) {
@@ -103,12 +123,18 @@ function Item({ item, inHundleDeletes, onHundleToggleItems }) {
 }
 
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding somes to your parking list</em>
+      </p>
+    );
   const numItems = items.length;
   const packedItems = items.filter((item) => item.packed).length;
   const percentage = Math.round((packedItems / numItems) * 100);
   return (
     <footer>
-      <em>
+      <em className="stats">
         {percentage === 100
           ? "You got every thing! ready to go."
           : `you have ${numItems} items on your list, and you aready packed${" "}
